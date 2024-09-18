635fce4a2519c87b50551151de77071385e2f209
ZXROVX <91070944+zxrovx@users.noreply.github.com>
2024-09-18 18:10:00 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Liliya Nethunter - Realme Trinket
+kernel.string=Zen Kernel - Realme Trinket
 do.devicecheck=0
 do.modules=0
 do.systemless=1
